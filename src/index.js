bc9456b4bc21d00d1bb23e1ec9c8eae19f03b039
ankit-mrexcalibur <55681020+ankit-mrexcalibur@users.noreply.github.com>
2021-12-31 14:04:57 +0530

    End

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+var name = "Ankit";
+var num = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}!</h1> <p>Your lucky number is {num}.</p>{" "}
+  </div>,
+  document.getElementById("root")
+);
